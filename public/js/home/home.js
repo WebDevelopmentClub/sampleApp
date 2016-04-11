17574489c220a5e7c71652faa18afd205f8316a9
romil robtsenkov <romilrobtsenkov@users.noreply.github.com>
2016-04-11 19:38:40 +0300

    added Topic service

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -3,10 +3,21 @@
 
     angular
     .module('app')
-    .controller('HomeController', ['$scope',
-    function($scope) {
+    .controller('HomeController', ['$scope','Topic',
+    function($scope, Topic) {
 
         //controller logic goes here
+
+        Topic.query()
+            .$promise.then(
+                function(data){
+                    console.log(data);
+                },
+                function(error){
+                    console.log(error);
+                }
+            );
+
 
     }]);
 }());
