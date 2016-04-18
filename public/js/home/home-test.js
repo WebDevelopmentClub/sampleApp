19937ed74c0c10d2ad25fa3c4bbb64e197a8b47d
romil robtsenkov <romilrobtsenkov@users.noreply.github.com>
2016-04-18 21:01:32 +0300

    added karma full test

--- public/js/home/home-test.js
+++ public/js/home/home-test.js
@@ -1,23 +1,46 @@
 describe('app', function() {
 
-    var HomeController, scope;
+  var HomeController, scope, mock$mdDialog, location;
 
-    beforeEach(inject(function ($rootScope, $controller) {
-        scope = $rootScope.$new();
-        HomeController = $controller("HomeController", {
-            $scope: scope,
+  mock$mdDialog =  {
+    show : jasmine.createSpy(),
+    hide : jasmine.createSpy()
+    };
 
-        });
-    }));
+  location = {path: jasmine.createSpy('path')};
 
-    describe('HomeController', function(){
 
-        //HomeController tests go here
-        it('should have HomeController to exist', function() {
-            //spec body
-            expect(HomeController).toBeDefined();
-            expect(scope.loading).toEqual(true);
-        });
+  beforeEach(module('app'), function($provide){
+      $provide.factory('$mdDialog', mock$mdDialog);
+  });
 
+  beforeEach(inject(function ($rootScope, $controller) {
+      scope = $rootScope.$new();
+      HomeController = $controller("HomeController", {
+          $scope: scope,
+          $mdDialog: mock$mdDialog,
+          $location: location,
+      });
+  }));
+
+  describe('HomeController', function(){
+
+    it('HomeController should exists', function() {
+      //spec body
+      expect(scope.loading).toEqual(true);
+      expect(HomeController).toBeDefined();
     });
+
+    it('openDialog should open dialog', function() {
+      //spec body
+      scope.openDialog();
+
+      expect(mock$mdDialog.show).toHaveBeenCalled();
+    });
+
+    it('trigger locaton path on navigateTo with paramater', function () {
+        scope.navigateTo(123);
+        expect(location.path).toHaveBeenCalledWith('/topic/' + 123);
+    });
+  });
 });
